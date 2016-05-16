133cdeaa77c0bd0acf5ad03da2c27c0701ae1ca3
yejiachen <yejia.chen.taipei@gmail.com>
2016-05-16 22:53:31 +0800

    add 5/14 learned

---
+++ 0514/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace ClassLibrary1
+{
+    public class Class123
+    {
+        public int example()
+        {
+            return 999;
+
+        }
+    }
+}
